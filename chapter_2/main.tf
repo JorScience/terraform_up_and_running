f240ea92dc5aa5255a62aad4e94466d425f9ca4d
Jorik Schra <jschra@deloitte.nl>
2021-12-30 10:19:21 +0100

    Chapter number adjustments

--- chapter_2/main.tf
+++ chapter_2/main.tf
@@ -2,33 +2,122 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "terraform-state" {
-    bucket = "terraform-up-and-running-jorik"
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+    # Allow inbound requests
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Allow all outbound requests
+    egress {
+        from_port = 0
+        to_port  = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id             = "ami-0c55b159cbfafe1f0"
+    instance_type        = "t2.micro"
+    security_groups      = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #! /bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
 
     lifecycle {
-        prevent_destroy = true
+        create_before_destroy = true
     }
+}
 
-    versioning {
-        enabled = true
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
     }
+}
 
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = 200
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb" "example" {
+    name                = "terraform-asg-example"
+    load_balancer_type  = "application"
+    subnets             = data.aws_subnet_ids.default.ids
+    security_groups     = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port              = 80
+    protocol          = "HTTP"
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
         }
     }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-    name = "terraform-up-and-running-locks"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
 
-    attribute {
-        name = "LockID"
-        type = "S"
+    condition {
+        path_pattern {
+            values = ["*"]
+        }
+    }
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
     }
 }
