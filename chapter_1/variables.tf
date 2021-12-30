8b9f12e75300e6cf6f437b1375497db1dcd3ceb1
Jorik Schra <jschra@deloitte.nl>
2021-12-30 09:59:57 +0100

    Chapter 2 final state

--- chapter_1/variables.tf
+++ chapter_1/variables.tf
@@ -4,7 +4,7 @@ variable "server_port" {
     default     = 8080
 }
 
-output "public_ip" {
-    value       = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
+output "alb_dns_name" {
+    value       = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
 }
